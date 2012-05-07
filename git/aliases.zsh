423285900ce14d6d119d75e9727ed327597cac55
Bradly Feeley <bradlyf@gmail.com>
2012-05-07 13:17:44 -0700

    Adding git aliases.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,14 +6,16 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gb='git branch'
+alias add='git add'
+alias branch='git branch'
+alias checkout='git checkout'
+alias cherry-pick='git cherry-pick'
+alias commit='git commit'
+alias d='git diff'
+alias fetch='git fetch'
+alias log="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias push='git push'
+alias rebase='git rebase'
+alias reset='git reset'
+alias s='git status'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
