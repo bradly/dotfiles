5ecc7d0a1cba136bb06db89e48cd8da21dbc81bf
Bradly Feeley <bradlyf@gmail.com>
2013-05-02 10:25:40 -0700

    Cleaning up git settigns.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,17 +7,26 @@ then
 fi
 
 alias add='git add'
+alias bisect='git bisect'
 alias branch='git branch'
 alias checkout='git checkout'
 alias cherry-pick='git cherry-pick'
+alias clone='git clone'
 alias commit='git commit'
 alias d='git diff'
+alias diff='git diff'
 alias fetch='git fetch'
-alias log="git log -n 20 --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias log="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias merge='git merge'
+alias pull='git pull'
 alias push='git push'
 alias rebase='git rebase'
 alias reset='git reset'
-alias s='git status'
+alias revert='git revert'
+alias reflog='git reflog'
 alias show='git show'
 alias stash='git stash'
+alias s='git status'
+alias status='git status'
+alias tag='git tag'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
