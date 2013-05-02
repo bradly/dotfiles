910ce394270426ab8de3d3a006ed557bceae39a5
Bradly Feeley <bradlyf@gmail.com>
2013-05-02 10:30:48 -0700

    Adding simple deploy dotfiles.

---
+++ simple_deploy/aliases.zsh
@@ -0,0 +1,1 @@
+alias sd='simple_deploy'
---
+++ simple_deploy/completion.zsh
@@ -0,0 +1,1 @@
+compctl -k "(clone create deploy destroy environments events execute instances list outputs parameters protect resources status template update)" simple_deploy
